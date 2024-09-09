7a5a0859d988bf8d5042fc5132aa55855643a521
kss <slupphaugkjell@gmail.com>
2024-09-09 21:51:15 +0200

    initial commit

--- R/generics_lavaan.R
+++
@@ -1,9 +0,0 @@
-#' @export
-parameter_estimates.lavaan <- function(object, ...) {
-  lavaan::parameterEstimates(object, ...)
-}
-
-
-isLavaanObject <- function(x) {
-  inherits(x, "lavaan")
-}
--- R/modsem-package.R
+++
@@ -1,9 +0,0 @@
-#' @keywords internal
-"_PACKAGE"
-
-## usethis namespace: start
-#' @name modsem
-#' @importFrom Rcpp sourceCpp
-#' @useDynLib modsem, .registration = TRUE
-## usethis namespace: end
-NULL
