6b40e5630faad2f8bf1151fee5ada592043cb670
kss2k <slupphaugkjell@gmail.com>
2025-02-03 08:36:23 +0100

    trying a third time

--- tests/testthat/test_multigroup.R
+++ tests/testthat/test_multigroup.R
@@ -1,16 +1,16 @@
 devtools::load_all()
-HS.model <- ' visual  =~ x1 + x2 + x3
+model <- ' visual  =~ x1 + x2 + x3
               textual =~ x4 + x5 + x6
               speed   =~ x7 + x8 + x9 
               visual ~ c(v_t1, v_t2) * textual + speed
               interaction := v_t2 - v_t1'
 
-fit_lavaan <- lavaan::sem(HS.model, 
+fit_lavaan <- lavaan::sem(model, 
                           data = lavaan::HolzingerSwineford1939, 
                           group = "school")
 
 # using modsem 
-fit_modsem <- modsem(HS.model, 
+fit_modsem <- modsem(model, 
                      data = lavaan::HolzingerSwineford1939, 
                      group = "school")
 
@@ -24,3 +24,13 @@ modsemEst <- lavaan::parameterEstimates(fit_modsem$lavaan)
 modsemEst[is.na(modsemEst)] <- -999
 testthat::expect_equal(lavaanEst, modsemEst)
 
+model <- ' visual  =~ x1 + x2 + x3
+              textual =~ x4 + x5 + x6
+              speed   =~ x7 + x8 + x9 
+              visual ~ textual + speed + speed:textual'
+
+
+# using modsem 
+fit_modsem <- modsem(model, 
+                     data = lavaan::HolzingerSwineford1939, 
+                     group = "school")
