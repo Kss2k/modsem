10749a54a9abfb6497ea0130e084174659601cd7
kss <slupphaugkjell@gmail.com>
2025-04-06 23:15:35 +0200

    initial test for efficient total likelihood

--- R/equations_lms.R
+++ R/equations_lms.R
@@ -87,6 +87,39 @@ estepLms <- function(model, theta, data, ...) {
 }
 
 
+total_dmvn_weighted <- function(X, mu, sigma, gamma) {
+  # X: n x d matrix of data
+  # mu: d-dimensional mean vector
+  # sigma: d x d covariance matrix
+  # gamma: n-dimensional vector of weights
+
+  n <- nrow(X)
+  d <- ncol(X)
+  
+  Gamma <- sum(gamma)
+  
+  # Weighted mean
+  x_bar <- colSums(X * gamma) / Gamma
+
+  # Weighted scatter matrix
+  X_centered <- X - matrix(x_bar, n, d, byrow = TRUE)
+  S_gamma <- t(X_centered) %*% (X_centered * gamma)
+
+  # Inverse and determinant
+  sigma_inv <- solve(sigma)
+  log_det_sigma <- determinant(sigma, logarithm = TRUE)$modulus
+
+  # Quadratic terms
+  trace_term <- sum(sigma_inv * S_gamma)
+  mean_diff <- matrix(x_bar - mu, nrow = 1)
+  mahalanobis_term <- Gamma * (mean_diff %*% sigma_inv %*% t(mean_diff))
+
+  # Final log-likelihood
+  log_likelihood <- -0.5 * (Gamma * d * log(2 * pi) + Gamma * log_det_sigma + trace_term + mahalanobis_term)
+  as.numeric(log_likelihood)
+}
+
+
 logLikLms <- function(theta, model, data, P, sign = -1, ...) {
   modFilled <- fillModel(model = model, theta = theta, method = "lms")
   k <- model$quad$k
@@ -100,6 +133,15 @@ logLikLms <- function(theta, model, data, P, sign = -1, ...) {
       log = TRUE
     ) * P[, i])
     lls
+    lls2 <- total_dmvn_weighted(X = data,
+      mu = muLmsCpp(model = modFilled, z = V[i, ]),
+      sigma = sigmaLmsCpp(model = modFilled, z = V[i, ]),
+      gamma = P[, i])
+
+    lls2
+
+    if (round(lls2, 6) != round(lls, 6)) browser()
+    lls2
   }) |> sum()
   sign * r
 }
