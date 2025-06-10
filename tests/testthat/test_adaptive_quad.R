d917e2c2d474863bc47e682fe8d0ac394ee37b1c
kss <slupphaugkjell@gmail.com>
2025-06-11 01:36:59 +0200

    increase adaptive.frequency in last test in

--- tests/testthat/test_adaptive_quad.R
+++ tests/testthat/test_adaptive_quad.R
@@ -87,7 +87,7 @@ testthat::expect_warning({
   # adaptive quadrature, as all the nodes bring some value
   lms4 <- modsem(nlsem, data = jordan, method = "lms", 
                  adaptive.quad=TRUE, calc.se=FALSE,
-                 nodes = 15, mean.observed = FALSE, adaptive.frequency=3)
+                 nodes = 15, mean.observed = FALSE, adaptive.frequency=10)
 }, regex = "It is recommended that you have at least 16 nodes.*")
 
 summary(lms4)
