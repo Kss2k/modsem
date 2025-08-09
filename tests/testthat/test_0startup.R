b68f1736c2d55cbf9d76a3a34bb7279a4480d455
kss <slupphaugkjell@gmail.com>
2025-08-09 17:11:02 +0200

    Clean up test-workflow

--- tests/testthat/test_0startup.R
+++ tests/testthat/test_0startup.R
@@ -1,6 +1,9 @@
+devtools::load_all()
+
 OK_OpenMP <- checkOpenMP_Cpp()
 startupMessages <- purrr::quietly(devtools::load_all)()$messages[-1L]
 startupMessage <- paste0("    ", startupMessages, collapse = "\n")
+
 
 printSessionInfo <- function() {
   info <- sessionInfo()
@@ -8,28 +11,39 @@ printSessionInfo <- function() {
   info$openmp <- if (OK_OpenMP) "AVAILABLE" else "NOT AVAILABLE"
 
   show <- (sapply(info, FUN = length) == 1L) & names(info) != "locale"
-  lhs <- format(names(info[show]), justify = "left")
-  rhs <- format(unlist(info[show]), justify = "right")
+  rhs <- names(info[show])
+  lhs <- unlist(info[show])
+
+  # cut paths
+  largePath <- stringr::str_count(rhs, pattern = "/") > 3L
+  browser()
+  getstem <- \(path, i) stringr::str_split_i(path, pattern = "/", i = i)
+  rhs[ispath] <- paste0(
+
+    stringr::str_split_i(rhs[ispath], pattern = "/", i = -1)
+  )
+
+  flhs <- format(lhs, justify = "left")
+  frhs <- format(rhs, justify = "right")
 
   width <- options("width")[[1L]]
   space <- strrep(" ", max(2L, width - max(nchar(lhs)) - max(nchar(rhs))))
   sep1  <- paste0(strrep("\u2500", width), "\n")
   sep2  <- paste0(strrep("\u2550", width), "\n")
 
-
+  cat("\n")
   cat(sep1)
   cat("RUNNING TESTS CASES\n")
   cat(sep2)
 
-  for (i in seq_along(lhs)) cat(lhs[i], space, rhs[i], "\n", sep = "")
+  for (i in seq_along(lhs))
+    cat(flhs[i], space, frhs[i], "\n", sep = "")
 
   cat(sep1)
   cat("STARTUP MESSAGE:\n")
   cat(startupMessage)
 
-  cat(sep1)
-
-
+  cat(sep1, "\n")
 }
 
 printSessionInfo()
