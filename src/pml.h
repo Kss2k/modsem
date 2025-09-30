1761a44257fcb54eb1f796ca7f0186feb58fa54b
kss2k <slupphaugkjell@gmail.com>
2025-09-30 13:47:43 +0200

    add some pml utils

---
+++ src/pml.h
@@ -0,0 +1,19 @@
+#ifndef PML_H
+#define PML_H
+
+
+arma::vec fcc_vec_arma(const arma::vec& xj, const arma::vec& xk,
+                       const arma::vec& mj, const arma::vec& mk,
+                       const arma::vec& Sjj, const arma::vec& Skk, const arma::vec& Sjk);
+
+arma::vec foc_vec_arma(const arma::vec& xj, const arma::uvec& r,
+                       const arma::vec& mj, const arma::vec& mk,
+                       const arma::vec& Sjj, const arma::vec& Skk, const arma::vec& Sjk,
+                       const arma::vec& tau_k);
+
+arma::vec foo_vec_arma(const arma::uvec& r, const arma::uvec& s,
+                       const arma::vec& mj, const arma::vec& mk,
+                       const arma::vec& Sjj, const arma::vec& Skk, const arma::vec& Sjk,
+                       const arma::vec& tau_j, const arma::vec& tau_k);
+
+#endif
