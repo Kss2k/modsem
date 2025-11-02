e9847d2be95d3d50c8294f43209c74e94f1d7eb4
kss <slupphaugkjell@gmail.com>
2025-11-02 12:54:19 +0100

    Continue working on likelihood

--- src/equations_gsem.cpp
+++ src/equations_gsem.cpp
@@ -10,6 +10,7 @@
 
 struct GSEMModel {
   arma::mat lambda, tau, thresholds, Ie, gamma, psi, alpha, Y;
+  std::vector<arma::mat> Z;
   unsigned  k       = 0;
   arma::vec isordered;
 
@@ -18,6 +19,7 @@ struct GSEMModel {
     Rcpp::List matrices = modFilled["matrices"];
     Rcpp::List info     = modFilled["info"];
     Rcpp::List quad     = modFilled["quad"];
+    Rcpp::List quad_i   = modFilled["quad_i"];
 
     k  = Rcpp::as<unsigned>(quad["k"]);
 
@@ -32,11 +34,16 @@ struct GSEMModel {
     Thresholds = Rcpp::as<arma::mat>(matrices["thresholds"]);
     Y          = Rcpp::as<arma::mat>(matrices["data"]);
     isordered  = Rcpp::as<arma::vec>(matrices["isordered"]);
+
+
+    Z = std::vector(Rcpp::length(quad_i));
+    for (int q = 0; q < Rcpp::length(quad_i); q++)
+      Z[q] = Rcpp::as<arma::mat>(quad_i[q]);
   }
 
-  arma::vec expectedResponse(const arma::mat Zi) const {
-    arma::vec out(Zi.n_rows);
-    arma::vec ivec = arma::ones<arma::vec>(Zi.n_rows);
+  arma::vec expectedResponse(const arma::mat Zq) const {
+    arma::vec out(Zq.n_rows);
+    arma::vec ivec = arma::ones<arma::vec>(Zq.n_rows);
 
     const arma::mat L;
 
@@ -46,12 +53,12 @@ struct GSEMModel {
     }
 
     const arma::mat B = arma::inv(Ie - gamma);
-    return ivec * tau.t() + B * (alpha + Zi * L) * lambda;
+    return ivec * tau.t() + B * (alpha + Zq * L) * lambda;
   }
 
-  arma::vec getDensityZi(const arma::mat Zi) const {
-    arma::vec density = arma::ones<arma::vec>(Zi.n_rows);
-    const arma::vec v = expectedResponse(Zi);
+  arma::vec getDensityZq(const arma::mat Zq) const {
+    arma::vec density = arma::ones<arma::vec>(Zq.n_rows);
+    const arma::vec v = expectedResponse(Zq);
 
     for (int j = 0; j < Y.n_cols; j++) {
       const double sd = std::sqrt(Theta(j, j));
@@ -68,28 +75,28 @@ struct GSEMModel {
   }
 
   arma::vec Qi() {
-    for (int q = 0; q < quad.n_elems; q++) 
+    arma::vec density = arma::ones<arma::vec>(Z[q].n_rows);
+
+    for (int q = 0; q < quad.n_elems; q++)
+      density *= getDensityZq(Z[q]);
+
+    return density
+  }
+
+
+  arma::vec Q() {
 
   }
 
   LMSModel threadClone() const {
     LMSModel c = *this;    // shallow for everything (fast)
                            // Deep-copy ONLY what setParams()/lms_param can modify:
-    c.A     = arma::mat(A);
-    c.Oxx   = arma::mat(Oxx);
-    c.Oex   = arma::mat(Oex);
     c.Ie    = arma::mat(Ie);
-    c.lY    = arma::mat(lY);
-    c.lX    = arma::mat(lX);
-    c.tY    = arma::mat(tY);
-    c.tX    = arma::mat(tX);
-    c.Gx    = arma::mat(Gx);
-    c.Ge    = arma::mat(Ge);
-    c.a     = arma::mat(a);
-    c.beta0 = arma::mat(beta0);
+    c.Lamba = arma::mat(Lambda);
+    c.Gamma = arma::mat(Gamma);
+    c.alpha = arma::mat(alpha);
     c.Psi   = arma::mat(Psi);
-    c.d     = arma::mat(d);
-    c.e     = arma::mat(e);
+    c.Theta = arma::mat(Theta);
 
     return c;
   }
