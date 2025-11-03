cc690f30c885aaf5e018a50724dacac6be3f1dbd
kss2k <slupphaugkjell@gmail.com>
2025-11-03 11:13:14 +0100

    Check handling of `sampling.weights.normalization` in MG models

--- tests/testthat/test_sampling_weights_da.R
+++ tests/testthat/test_sampling_weights_da.R
@@ -10,15 +10,32 @@ m0 <- '
 set.seed(2934829)
 oneInt2 <- oneInt
 oneInt2$weights <- rnorm(NROW(oneInt2), mean = 1, sd = 0.2)
+oneInt2$group   <- sample(2, NROW(oneInt2), replace = TRUE)
 
 testthat::expect_no_condition({
   fit.lms.h0 <- modsem(m0, oneInt2, method = "lms", sampling.weights = "weights")
   fit.qml.h0 <- modsem(m0, oneInt2, method = "qml", sampling.weights = "weights")
+  fit.lms.h0.mg1 <- modsem(m0, oneInt2, method = "lms", group = "group", sampling.weights = "weights", sampling.weights.normalization = "total")
+  fit.lms.h0.mg2 <- modsem(m0, oneInt2, method = "lms", group = "group", sampling.weights = "weights", sampling.weights.normalization = "group")
 })
 
 testthat::expect_true(fit.lms.h0$iterations == 2L)
 testthat::expect_true(fit.qml.h0$iterations == 1L)
+testthat::expect_true(fit.lms.h0.mg1$iterations == 2L)
+testthat::expect_true(fit.lms.h0.mg2$iterations == 2L)
 
+w.g1.1 <- fit.lms.h0.mg1$model$models[[1L]]$data$weights
+w.g2.1 <- fit.lms.h0.mg1$model$models[[2L]]$data$weights
+w.g1.2 <- fit.lms.h0.mg2$model$models[[1L]]$data$weights
+w.g2.2 <- fit.lms.h0.mg2$model$models[[2L]]$data$weights
+
+testthat::expect_true(sum(w.g1.1) + sum(w.g2.1) == length(w.g1.1) + length(w.g2.1))
+testthat::expect_true(sum(w.g1.1) != length(w.g1.1))
+testthat::expect_true(sum(w.g2.1) != length(w.g2.1))
+
+testthat::expect_true(sum(w.g1.2) + sum(w.g2.2) == length(w.g1.2) + length(w.g2.2))
+testthat::expect_true(sum(w.g1.2) == length(w.g1.2))
+testthat::expect_true(sum(w.g2.2) == length(w.g2.2))
 
 m1 <- '
   X =~ x1 + x2 + x3
