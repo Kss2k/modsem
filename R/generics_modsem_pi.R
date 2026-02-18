6599ab3a36f53b86ad3229737092c568ad2b6878
kss2k <slupphaugkjell@gmail.com>
2026-02-18 11:51:50 +0100

    Fix `colon.pi` argument in `standardized_estimates()`

--- R/generics_modsem_pi.R
+++ R/generics_modsem_pi.R
@@ -300,6 +300,31 @@ standardized_estimates.modsem_pi <- function(object,
   std.errors  <- tolower(std.errors)
   std.errors  <- match.arg(std.errors)
   monte.carlo <- std.errors == "monte.carlo"
+ 
+  cleanOutputParTable <- function(parTable) {
+    if (colon.pi) {
+      elems <- object$elementsInProdNames
+
+      if (length(elems) && !"label" %in% colnames(parTable))
+        parTable$label <- ""
+
+      origLabels <- getParTableLabels(parTable, labelCol = "label")
+
+      for (xz in names(elems)) {
+        xzColon <- paste0(elems[[xz]], collapse = ":")
+        rmatch <- parTable$rhs == xz
+        lmatch <- parTable$lhs == xz
+
+        parTable[rmatch | lmatch, "label"] <- origLabels[rmatch | lmatch]
+
+        parTable[rmatch, "rhs"] <- xzColon
+        parTable[lmatch, "lhs"] <- xzColon # shouldn't be necessary, but just in case
+                                           # the user has done something weird...
+      }
+    }
+
+    parTable
+  }
 
   uncorrected <- \(object) rename(lavaan::standardizedSolution(object$lavaan, ...),
                                   est.std = "est")
@@ -328,7 +353,7 @@ standardized_estimates.modsem_pi <- function(object,
       solution$parTable
     }
 
-  } else return(uncorrected(object))
+  } else return(cleanOutputParTable(uncorrected(object)))
 
   parTable.ustd <- parameter_estimates(object)
 
@@ -344,13 +369,7 @@ standardized_estimates.modsem_pi <- function(object,
 
   parTable.std <- correction(object)
 
-  if (!colon.pi) {
-    rm <- \(x) stringr::str_remove_all(x, ":")
-    parTable.std$rhs <- rm(parTable.std$rhs)
-    parTable.std$lhs <- rm(parTable.std$lhs)
-  }
-
-  parTable.std
+  cleanOutputParTable(parTable.std)
 }
 
 
--- tests/testthat/test_moderated_mediation.R
+++ tests/testthat/test_moderated_mediation.R
@@ -16,4 +16,18 @@ plot_jn(x = "ATT", z = "PBC", y = "BEH", model = est_lms, type = "indirect")
 plot_jn(x = "ATT", z = "PBC", y = "BEH", model = est_lms, type = "total")
 
 est_dblcent <- modsem(tpb, TPB, method = "dblcent")
+p
+
+
+model <- '
+  X =~ att1 + att2 + att3 + att4 + att5
+  W =~ pbc1 + pbc2 + pbc3
+  M =~ int1 + int2 + int3
+  Y =~ b1 + b2
+
+  M ~ a*X
+  Y ~ c1*X + c2*W + c3*X:W + b1*M + b2*M:W
+'
+
+fit <- modsem(model, data = TPB, method = "lms")
 plot_jn(x = "ATT", z = "PBC", y = "BEH", model = est_dblcent, type = "indirect")
